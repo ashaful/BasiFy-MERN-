ce82d3f291f203a958ce7f59566043f50490b1d1
Md Ashaful Azim <95497643+ashaful@users.noreply.github.com>
2024-09-24 01:57:02 +0600

    Js Day-2
    variable. constant, operator

---
+++ JavaScripts/Day-2/script.js
@@ -0,0 +1,84 @@
+//variable
+//case sensative
+
+var a = 20;
+let b = a;
+
+
+document.write(a);
+document.write(b);
+
+//both answer = 20 becs a assign to b 
+
+var a = 30;
+document.write(a);
+document.write(b);
+// 30 20
+
+//
+
+var c = a + b;
+
+document.write(c);
+//2020 3020 50
+
+//Constants
+
+//var a = 20; // fixed value always 
+
+//operator
+
+// % remainder 
+
+2**3 //power
+
+//a++ (post Inc) / a = a+1 / ++a //(pre Inc) //increment  
+a__ / a = a -1 / --a//dicriment 
+
+//string
+ var a= "akib"
+ var b= "mohammad"
+
+ a+" "+b
+
+ "akib"+3+4  = akib34
+ 3+4+"akib"  = "7akib"
+
+ //logical
+
+ // == means equel to // 
+ 4 == "4" true  4 === "4"  false
+//  !=  means not equal to  
+
+4 !== 4 false
+4 !== "4" true
+
+//logical 
+
+//    or  ||    
+true || true  true 
+true || false true 
+false || true true 
+false || false false 
+
+// && and 
+true && true true 
+true && false false 
+
+
+// assignment operator  =
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
