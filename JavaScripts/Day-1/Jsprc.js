555fe1754b81df48ebed3f09fb0512b58308c9d1
Md Ashaful Azim <95497643+ashaful@users.noreply.github.com>
2024-09-23 23:27:23 +0600

    Started learning JavaScript

---
+++ JavaScripts/Day-1/Jsprc.js
@@ -0,0 +1,5 @@
+var a =10;
+let b = "akib";
+
+console.log(a);
+console.log(b);
---
+++ JavaScripts/Day-1/js/script.js
@@ -0,0 +1,16 @@
+document.getElementById('root').innerHTML =  
+		" Displaying data in javascripts by innerHTML" ;
+		
+		alert(" javascripts alert!!!");
+		//document.write("second option for showing output"); 
+
+		console.log("output at console");
+
+//use for comment  
+/* use for multiple comment */
+
+		var x ;
+
+		x = prompt("your name: "); // prompt use for user input 
+		document.write(x);
+
